ee1162d45e7014e3a967999b1c64a0e710801bac
VARADRAJ GHOLAP <varadgholap163@gmail.com>
2025-01-13 09:00:16 +0530

    identify capital letters in string

---
+++ capitalLetterCount.cpp
@@ -0,0 +1,37 @@
+class Solution
+{
+public:
+    bool detectCapitalUse(string word)
+    {
+        int n = word.size();
+        int capitalCount = 0;
+
+        // Count the number of uppercase letters
+        for (char ch : word)
+        {
+            if (isupper(ch))
+            {
+                capitalCount++;
+            }
+        }
+
+        // Check valid cases
+        if (capitalCount == n)
+        {
+            // All letters are capitals
+            return true;
+        }
+        else if (capitalCount == 0)
+        {
+            // All letters are not capitals
+            return true;
+        }
+        else if (capitalCount == 1 && isupper(word[0]))
+        {
+            // Only the first letter is capital
+            return true;
+        }
+
+        return false; // Invalid usage of capitals
+    }
+};
