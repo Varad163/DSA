271a75145c5fdaf7ba8c9297208c44a5548f068f
VARADRAJ GHOLAP <varadgholap163@gmail.com>
2025-01-13 08:47:59 +0530

    fibonacci.cpp

---
+++ fibonacci.cpp
@@ -0,0 +1,26 @@
+//fibonnacci upto n digits
+
+
+
+class Solution
+{
+public:
+    int fib(int n)
+    {
+        if (n <= 1)
+            return n;
+
+        int prev2 = 0;
+        int prev1 = 1;
+        int current;
+
+        for (int i = 2; i <= n; ++i)
+        {
+            current = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = current;
+        }
+
+        return current;
+    }
+};
