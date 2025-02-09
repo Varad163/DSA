22b59db1904b33758223f4340c87c8c3c2f34351
VARADRAJ GHOLAP <varadgholap163@gmail.com>
2025-02-09 22:46:20 +0530

    BitManipulation

---
+++ Bit-Manipulation/examplecode.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A = 5, B = 3;
+
+    cout << "A & B: " << (A & B) << endl;   // AND
+    cout << "A | B: " << (A | B) << endl;   // OR
+    cout << "A ^ B: " << (A ^ B) << endl;   // XOR
+    cout << "~A: " << (~A) << endl;         // NOT
+    cout << "A << 1: " << (A << 1) << endl; // Left Shift
+    cout << "A >> 1: " << (A >> 1) << endl; // Right Shift
+
+    return 0;
+}
