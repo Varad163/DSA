6e69ead8480fd70724ab639fb6f66434c124ebbe
VARADRAJ GHOLAP <varadgholap163@gmail.com>
2025-02-25 23:55:55 +0530

    recursion

---
+++ Recursion/recursion4.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void generateSubsets(string &s, string current, int index, vector<string> &result)
+{
+    if (index == s.length())
+    {
+        result.push_back(current);
+        return;
+    }
+
+    // Exclude the current character
+    generateSubsets(s, current, index + 1, result);
+
+    // Include the current character
+    generateSubsets(s, current + s[index], index + 1, result);
+}
+
+int main()
+{
+    string s = "abc";
+    vector<string> subsets;
+
+    generateSubsets(s, "", 0, subsets);
+
+    // Print all subsets
+    for (const string &subset : subsets)
+    {
+        cout << "\"" << subset << "\" ";
+    }
+
+    return 0;
+}
