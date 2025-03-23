f513a3729491fb5b558d0a931a85279cc53c7e8a
VARADRAJ GHOLAP <varadgholap163@gmail.com>
2025-03-23 23:56:00 +0530

    update

--- Recursion/recursion6.cpp
+++ Recursion/recursion6.cpp
@@ -6,7 +6,7 @@ int factorial(int n)
     // Base case
     if (n == 0 || n == 1)
         return 1;
-    // Recursive case
+    
     return n * factorial(n - 1);
 }
 
@@ -18,3 +18,7 @@ int main()
     cout << "Factorial of " << num << " is " << factorial(num) << endl;
     return 0;
 }
+
+
+
+
