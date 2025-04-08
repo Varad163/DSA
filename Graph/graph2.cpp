f9942a059efc2112329567b501f1d375e7981478
VARADRAJ GHOLAP <varadgholap163@gmail.com>
2025-04-08 23:57:52 +0530

    grapg

--- Graph/graph2.cpp
+++ Graph/graph2.cpp
@@ -72,16 +72,3 @@ public:
     }
 };
 
-int main()
-{
-    Graph g(5);
-    g.addEdge(0, 1);
-    g.addEdge(0, 2);
-    g.addEdge(1, 3);
-    g.addEdge(1, 4);
-
-    g.BFS(0);
-    g.DFS(0);
-
-    return 0;
-}
