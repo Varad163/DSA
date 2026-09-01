e9dd36f28706687e11c00edad830614b5bd71069
VARADRAJ GHOLAP <137861814+Varad163@users.noreply.github.com>
2026-09-01 23:53:24 +0530

    Time: 655 ms (60.20%) | Memory: 265.1 MB (48.98%) - LeetSync

--- 3870-minimum-moves-to-clean-the-classroom/minimum-moves-to-clean-the-classroom.cpp
+++ 3870-minimum-moves-to-clean-the-classroom/minimum-moves-to-clean-the-classroom.cpp
@@ -4,95 +4,74 @@ public:
         int m = classroom.size();
         int n = classroom[0].size();
 
-        int sr, sc;
-        int cnt = 0;
-
+        int sr, sc, cnt = 0;
         vector<vector<int>> id(m, vector<int>(n, -1));
 
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
                 if(classroom[i][j] == 'S') {
                     sr = i;
                     sc = j;
                 }
-
                 if(classroom[i][j] == 'L') {
                     id[i][j] = cnt++;
                 }
             }
         }
 
-        int totalMask = (1 << cnt) - 1;
-
-        int states = m * n * (energy + 1) * (1 << cnt);
+        int M = 1 << cnt;
+        int states = m*n*(energy+1)*M;
 
         vector<bool> vis(states, false);
-
         queue<tuple<int,int,int,int>> q;
 
-        q.push({sr, sc, energy, totalMask});
+        q.push({sr, sc, energy, M-1});
 
-        int start = (((sr * n + sc) * (energy + 1) + energy)
-                    * (1 << cnt)) + totalMask;
-
+        int start = (((sr*n+sc)*(energy+1)+energy)*M)+(M-1);
         vis[start] = true;
 
         int moves = 0;
-
-        int dr[] = {-1, 1, 0, 0};
-        int dc[] = {0, 0, -1, 1};
+        int dr[] = {-1,1,0,0};
+        int dc[] = {0,0,-1,1};
 
         while(!q.empty()) {
-
             int sz = q.size();
 
             while(sz--) {
-
-                auto [r, c, e, mask] = q.front();
+                auto [r,c,e,mask] = q.front();
                 q.pop();
 
-                if(mask == 0) {
+                if(mask == 0)
                     return moves;
-                }
 
-                if(e == 0) {
+                if(e == 0)
                     continue;
-                }
 
-                for(int k = 0; k < 4; k++) {
-
+                for(int k=0; k<4; k++) {
                     int nr = r + dr[k];
                     int nc = c + dc[k];
 
-                    if(nr < 0 || nr >= m || nc < 0 || nc >= n) {
+                    if(nr<0 || nr>=m || nc<0 || nc>=n)
                         continue;
-                    }
 
-                    if(classroom[nr][nc] == 'X') {
+                    if(classroom[nr][nc] == 'X')
                         continue;
-                    }
 
-                    int ne = e - 1;
+                    int ne = e-1;
                     int nmask = mask;
 
-                    if(classroom[nr][nc] == 'R') {
+                    if(classroom[nr][nc] == 'R')
                         ne = energy;
-                    }
 
                     if(classroom[nr][nc] == 'L') {
-                        int bit = id[nr][nc];
-                        nmask = nmask & ~(1 << bit);
+                        nmask &= ~(1 << id[nr][nc]);
                     }
 
-                    int state = (((nr * n + nc) * (energy + 1) + ne)
-                                * (1 << cnt)) + nmask;
+                    int state = (((nr*n+nc)*(energy+1)+ne)*M)+nmask;
 
                     if(!vis[state]) {
-
                         vis[state] = true;
-
-                        q.push({nr, nc, ne, nmask});
+                        q.push({nr,nc,ne,nmask});
                     }
                 }
             }
